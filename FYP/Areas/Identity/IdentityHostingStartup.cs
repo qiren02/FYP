99d40495766daaf684037f71f11dd0acca7d9543
qiren <qiren@QIREN02>
2022-07-01 21:39:48 +0800

    Add project files.

---
+++ FYP/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FYP.Areas.Identity.Data;
+using FYP.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FYP.Areas.Identity.IdentityHostingStartup))]
+namespace FYP.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<FYPContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("FYPContextConnection")));
+
+                services.AddDefaultIdentity<FYPUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<FYPContext>();
+            });
+        }
+    }
+}
